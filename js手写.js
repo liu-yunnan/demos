0163cbf4abf9f9d751a7c0678b38aaea2dc11229
Liu_yunnan <1792147770@qq.com>
2022-08-27 16:55:15 +0800

    函数

--- js手写.js
+++ js手写.js
@@ -52,6 +52,7 @@ function flatten1 (arr) {
   // some 不改变原数组
   // 不对空数组进行访问
   // 有一个元素满足条件，则表达式返回true , 剩余的元素不会再执行检测。
+  // 遍历原数组，若arr中含有数组则使用一次扩展运算符，直至没有为止。
   while (arr.some(item => Array.isArray(item))) {
     arr = [].concat(...arr)
   }
@@ -107,3 +108,224 @@ let copyObj1 = deepClone(obj)
 console.log('简易版深拷贝', copyObj1 === obj)
 let copyObj2 = deepClonePlus(obj)
 console.log('plus深拷贝', copyObj2 === obj)
+
+// 浅拷贝
+function shallowCopy (obj) {
+  if (typeof obj !== 'object') return obj
+  let newObj = Array.isArray(obj) ? [] : {}
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      newObj[key] = obj[key]
+    }
+  }
+  return newObj
+}
+// 测试用例
+let copyObj3 = shallowCopy(obj)
+// console.log('浅拷贝', copyObj3 === obj)
+
+// 类数组转数组
+const arrayLike = { 1: 'a', 4: 'd', length: 9 }
+// 可迭代对象使用使用扩展符
+// 不可迭代对象使用 Array.prototype.slice.call() 和 Array.from()
+// let array = [...arrayLike] 
+// let array = Array.from(arrayLike)
+// let array = Array.prototype.slice.call(arrayLike)
+// console.log(array)
+
+// 实现数组原型方法
+// forEach
+let sum = 0
+// arr.forEach(function (item) {
+//   sum += item
+// })
+
+Array.prototype.forEach1 = function (callback, thisArg) {
+  if (this == null) {
+    throw new TypeError('this is null or no defined')
+  }
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + 'is not function')
+  }
+  // debugger
+  // this当前传入的数组
+  let O = new Object(this)
+  // 意义：为了保证转换后的值为正整数。
+  const len = O.length >>> 0
+  let k = 0
+  for (let k = 0; k < len; k++) {
+    if (k in O) {
+      // value,index,array
+      callback.call(thisArg, O[k], k, O)
+    }
+  }
+}
+arr.forEach1(function (item) {
+  sum += item
+})
+console.log('forEach', sum)
+
+// map 
+Array.prototype.map1 = function (callback, thisArg) {
+  if (this == null) {
+    throw new TypeError('this is null or no defined')
+  }
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + 'is not function')
+  }
+  const o = new Object(this)
+  let len = o.length >>> 0
+  let newArr = []
+  for (let k = 0; k < len; k++) {
+    if (k in o) {
+      newArr[k] = callback.call(thisArg, o[k], k, o)
+    }
+  }
+  return newArr
+}
+
+let arrMap = arr.map1((item) => item * 2)
+console.log('map', arrMap)
+
+// filter
+Array.prototype.filter1 = function (callback, thisArg) {
+  if (this == null) {
+    throw new TypeError('this is null or no defined')
+  }
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + 'is not function')
+  }
+  const o = new Object(this)
+  let len = o.length >>> 0
+  let newArr = []
+  for (let k = 0; k < len; k++) {
+    if (callback.call(thisArg, o[k], k, o)) {
+      newArr.push(o[k])
+    }
+  }
+  return newArr
+}
+let arrFilter = arr.filter1((item) => item < 5)
+console.log('filter', arrFilter)
+
+// some
+Array.prototype.filter1 = function (callback, thisArg) {
+  if (this == null) {
+    throw new TypeError('this is null or no defined')
+  }
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + 'is not function')
+  }
+  const o = new Object(this)
+  let len = o.length >>> 0
+  let newArr = []
+  for (let k = 0; k < len; k++) {
+    if (callback.call(thisArg, o[k], k, o)) {
+      return true
+    }
+  }
+  return false
+}
+let arrSome = arr.some((item) => item > 5)
+console.log('some', arrSome)
+
+// reduce
+Array.prototype.reduce1 = function (callback, initialvalue) {
+  if (this == null) {
+    throw new TypeError('this is null or no defined')
+  }
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + 'is not function')
+  }
+  const o = new Object(this)
+  let len = o.length >>> 0
+  let k = 0
+  let accumulator = initialvalue
+  // 如果初始值为undefined，则需要在数组中找数组的第一个有效值
+  // debugger
+  if (accumulator === undefined) {
+    while (k < len && !(k in o)) {
+      k++
+    }
+    if (k >= len) {
+      // 没有初始值的空数组
+      throw new TypeError('Reduce of empty array with no initial value')
+    }
+    // 将第一个有效值给到初始值
+    accumulator = o[k++]
+  }
+  while (k < len) {
+    if (k in o) {
+      accumulator = callback(accumulator, o[k], k, o)
+    }
+    k++
+  }
+  return accumulator
+}
+
+let numbers = [15.5, 2.3, 1.1, 4.7]
+
+function getSum (total, num) {
+  return total + Math.round(num)
+}
+console.log('reduce', numbers.reduce1(getSum))
+
+// call
+Function.prototype.call1 = function (context, ...args) {
+  // debugger
+  // console.log(context)
+  if (typeof this !== 'function') {
+    throw new TypeError('this is not a function')
+  }
+  context = context || window
+  let fn = Symbol('fn')
+  context[fn] = this
+  // console.log(context[fn])
+  let res = context[fn](...args)
+  delete context[fn]
+  return res
+}
+
+// apply
+Function.prototype.apply1 = function (context, arr) {
+  if (typeof this !== 'function') {
+    throw new TypeError('this is not a function')
+  }
+  context = context || window
+  let fn = Symbol('fn')
+  context[fn] = this
+  let res = context[fn](...arr)
+  delete context[fn]
+  return res
+}
+
+// bind
+Function.prototype.bind1 = function (context, ...args) {
+  // debugger
+  let that = this  // 保存原函数（this）
+  if (typeof that !== "function") {
+    throw new TypeError("this is not function")
+  }
+  return function F (...innerArgs) {
+    // 判断是否是 new 构造函数
+    // 由于这里是调用的 call 方法，因此不需要判断 context 是否为空
+    return that.call(context, ...args, ...innerArgs)
+    // return that.call(this instanceof F ? this : context, ...args, ...innerArgs)
+  }
+}
+
+// 测试
+function add (a, b) {
+  // console.log(this);
+  return a + b + this.c
+}
+window.c = 10
+const objTest = {
+  c: 20
+}
+console.log('call', add.call1(undefined, 30, 40)) // 指向window 结果80
+console.log('call', add.call1(objTest, 30, 40)) // 指向obj 结果90
+console.log('apply', add.apply1(undefined, [30, 40])) // 指向window 结果80
+console.log('apply', add.apply1(objTest, [30, 40])) // 指向obj 结果90
+let fn1 = add.bind1(objTest, 30, 40)
+console.log('bind', fn1())
